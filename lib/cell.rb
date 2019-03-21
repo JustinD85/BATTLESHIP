e51b65678620cf0c17f724db9876225add7575c0
Vince Carollo <vincecarollo@gmail.com>
2019-03-21 13:58:33 -0600

    Apply changes recommended by instructer
    Co-authored-by: Justin <justin@baal-software.com>

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,10 +1,10 @@
 class Cell
   attr_reader :coordinate, :ship
-  
+
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
-    @fire_upon = false
+    @fired_upon = false
   end
 
   def empty?
@@ -16,33 +16,25 @@ class Cell
   end
 
   def fired_upon?
-    @fire_upon
+    @fired_upon
   end
 
   def fire_upon
     @ship.hit if @ship && !fired_upon?
-    @fire_upon = true
+    @fired_upon = true
   end
 
   def render(should_show = false)
-    case
-    when should_show && @ship
+    if should_show && @ship
       "S"
-    when fired_upon? && empty?
+    elsif fired_upon? && empty?
       "M"
-    when fired_upon? && @ship.sunk?
+    elsif fired_upon? && @ship.sunk?
       "X"
-    when fired_upon? && !empty?
+    elsif fired_upon? && !empty?
       "H"
     else
       "."
     end
-
-    # Unsure which is clearer
-    # return "S" if should_show && @ship
-    # return "M" if fired_upon? && empty?
-    # return "X" if fired_upon? && @ship.sunk?
-    # return "H" if fired_upon? && !empty?
-    # return "." if !fired_upon?
   end
 end
